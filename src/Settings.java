90f2ce990a82310d60f1a0196809fb05e96948c9
Emre Kara <emre1702@mail.uni-paderborn.de>
2017-10-25 01:34:52 +0200

    check if settings is available + add comment

--- src/Settings.java
+++ src/Settings.java
@@ -5,8 +5,12 @@ import org.json.simple.parser.JSONParser;
 
 class Settings {
 	static String token;
-	static String prefix;
+	static String prefix = "!";
 	
+	/**
+	 * Loads the settings from discordbot.log.
+	 * To disable a settings just delete it or change the value to 0.
+	 */
 	static void loadSettings ( ) {
 		JSONParser parser = new JSONParser();
 		
@@ -15,15 +19,25 @@ class Settings {
 			
 			JSONObject jsonObject = (JSONObject) obj;
 			token = (String) jsonObject.get( "token" );
-			prefix = (String) jsonObject.get( "prefix" );
 			
-			Channels.languageChannelID = (Long) jsonObject.get( "languageChannelID" );
-			Channels.musicChannelID = (Long) jsonObject.get( "musicChannelID" );
-			Channels.greetUserChannelID = (Long) jsonObject.get( "greetUserChannelID" );
-			Roles.musicbotUserID = (Long) jsonObject.get( "musicbotUserID" );
-			Roles.germanRoleID = (Long) jsonObject.get( "germanRoleID" );
-			Roles.englishRoleID = (Long) jsonObject.get( "englishRoleID" );
-			Roles.turkishRoleID = (Long) jsonObject.get( "turkishRoleID" );
+			if ( jsonObject.containsKey( "prefix" ) )
+				prefix = (String) jsonObject.get( "prefix" );
+			
+			if ( jsonObject.containsKey( "languageChannelID" ) )
+				Channels.languageChannelID = (Long) jsonObject.get( "languageChannelID" );
+			if ( jsonObject.containsKey( "musicChannelID" ) )
+				Channels.musicChannelID = (Long) jsonObject.get( "musicChannelID" );
+			if ( jsonObject.containsKey( "greetUserChannelID" ) )
+				Channels.greetUserChannelID = (Long) jsonObject.get( "greetUserChannelID" );
+			if ( jsonObject.containsKey( "musicbotUserID" ) )
+				Roles.musicbotUserID = (Long) jsonObject.get( "musicbotUserID" );
+			if ( jsonObject.containsKey( "germanRoleID" ) )
+				Roles.germanRoleID = (Long) jsonObject.get( "germanRoleID" );
+			if ( jsonObject.containsKey( "englishRoleID" ) )
+				Roles.englishRoleID = (Long) jsonObject.get( "englishRoleID" );
+			if ( jsonObject.containsKey( "turkishRoleID" ) )
+				Roles.turkishRoleID = (Long) jsonObject.get( "turkishRoleID" );
+			
 			ServerEmoji.whatcode = (Long) jsonObject.get( "whatEmojiID" );
 			ServerEmoji.what  = "<:what:"+ServerEmoji.whatcode+">";
 			ServerEmoji.hahacode = (Long) jsonObject.get( "hahaEmojiID" );
