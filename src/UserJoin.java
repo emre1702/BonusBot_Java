ef7741e5fa552412e6c8a023fcdfad48948e6a7d
emre1702 <emre1702@live.de>
2017-10-24 03:19:09 +0200

    send welcome message in greetUserChannel for the joined user

---
+++ src/UserJoin.java
@@ -0,0 +1,23 @@
+import sx.blah.discord.api.events.IListener;
+import sx.blah.discord.handle.impl.events.guild.member.UserJoinEvent;
+import sx.blah.discord.handle.obj.IGuild;
+
+class UserJoin implements IListener<UserJoinEvent> {
+
+	@Override
+	public void handle ( UserJoinEvent event ) {
+		if ( Channels.greetUserChannelID != 0 ) {
+			IGuild guild = event.getGuild();
+			int amountonserver = guild.getTotalMemberCount();
+			String suffix = amountonserver == 1 ? "st" : 
+				( amountonserver == 2 ? "nd" :
+				( amountonserver == 3 ? "rd" : "th" ) );
+			Util.sendMessage( guild.getChannelByID( Channels.greetUserChannelID ), "Welcome "+event.getUser().mention()
+					+"!\nYou are the "+amountonserver+suffix+" user "+ServerEmoji.tada
+					+"\nPlease read 'informations' in 'important' category." );
+			
+		}
+		
+	}
+
+}
