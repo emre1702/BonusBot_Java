ef5300279eb27ba1104cc42c744b82d1d3d069a1
Emre Kara <emre1702@mail.uni-paderborn.de>
2017-10-25 01:34:19 +0200

    default value to -1 (instead 0) + add comment

--- src/Roles.java
+++ src/Roles.java
@@ -6,16 +6,25 @@ import sx.blah.discord.handle.obj.IUser;
 
 class Roles {
 	
-	static long germanRoleID;
-	static long englishRoleID;
-	static long turkishRoleID;
-	static long musicbotUserID;
+	static long germanRoleID = -1;
+	static long englishRoleID = -1;
+	static long turkishRoleID = -1;
+	static long musicbotUserID = -1;
 	
+	/**
+	 * Checks if the specific user in the guild has the right to play music.
+	 * @param user The user which we want to check.
+	 * @param guild The guild where the user wants to play music.
+	 * @return If the user has the right to play music. Returns true if musicbotUserID is not defined in discordbot.conf.
+	 */
 	static boolean canPlayMusic ( IUser user, IGuild guild ) {
-		List<IRole> roles = user.getRolesForGuild( guild );
-		if ( roles.contains( guild.getRoleByID( musicbotUserID ) ) )
+		if ( musicbotUserID != -1 ) {
+			List<IRole> roles = user.getRolesForGuild( guild );
+			if ( roles.contains( guild.getRoleByID( musicbotUserID ) ) )
+				return true;
+			else
+				return false;
+		} else 
 			return true;
-		else
-			return false;
 	}
 }
