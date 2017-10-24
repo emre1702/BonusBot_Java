ee806a67733e65f718e1ae89a3997a792d3b5f01
Emre Kara <emre1702@mail.uni-paderborn.de>
2017-10-25 01:14:00 +0200

    add comments

--- src/Channels.java
+++ src/Channels.java
@@ -6,10 +6,20 @@ class Channels {
 	static long musicChannelID = 0;
 	static long greetUserChannelID = 0;
 	
+	/**
+	 * Checks if the ID is the ID of the language-channel.
+	 * @param ID The ID to be checked.
+	 * @return If the ID is the languageChannelID. Returns true when there is no language-channel.
+	 */
 	static boolean isLanguageChannel ( final long ID ) {
 		return ( languageChannelID == 0 || ID == languageChannelID );
 	}
 	
+	/**
+	 * Checks if the ID is the ID of the music-channel.
+	 * @param ID The ID to be checked.
+	 * @return If the ID is the musicChannelID. Returns true when there is no music-channel.
+	 */
 	static boolean isMusicChannel ( final long ID ) {
 		return ( musicChannelID == 0 || ID == musicChannelID );
 	}
