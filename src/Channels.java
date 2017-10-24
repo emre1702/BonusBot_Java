aecf6fa8b32a27e92e9d8620d51bed0a4559318a
Emre Kara <emre1702@mail.uni-paderborn.de>
2017-10-25 01:33:03 +0200

    default value to -1 (instead 0)

--- src/Channels.java
+++ src/Channels.java
@@ -2,25 +2,25 @@ class Channels {
 	
 	/* The channel where you will be able to add yourself the rights 
 	 * to read, write and talk in your language channels */
-	static long languageChannelID = 0;
-	static long musicChannelID = 0;
-	static long greetUserChannelID = 0;
+	static long languageChannelID = -1;
+	static long musicChannelID = -1;
+	static long greetUserChannelID = -1;
 	
 	/**
 	 * Checks if the ID is the ID of the language-channel.
 	 * @param ID The ID to be checked.
-	 * @return If the ID is the languageChannelID. Returns true when there is no language-channel.
+	 * @return If the ID is the languageChannelID. Returns true if languageChannelID is not defined in discordconf.log.
 	 */
 	static boolean isLanguageChannel ( final long ID ) {
-		return ( languageChannelID == 0 || ID == languageChannelID );
+		return ( languageChannelID == -1 || ID == languageChannelID );
 	}
 	
 	/**
 	 * Checks if the ID is the ID of the music-channel.
 	 * @param ID The ID to be checked.
-	 * @return If the ID is the musicChannelID. Returns true when there is no music-channel.
+	 * @return If the ID is the musicChannelID. Returns true if musicChannelID is not defined in discordconf.log.
 	 */
 	static boolean isMusicChannel ( final long ID ) {
-		return ( musicChannelID == 0 || ID == musicChannelID );
+		return ( musicChannelID == -1 || ID == musicChannelID );
 	}
 }
