b4d3aa06a7cadd8c9d060114f571b6171a987e7d
Emre Kara <emre1702@live.de>
2018-03-26 21:41:45 +0200

    Mention informations-Channel on UserJoinEvent

--- src/bonusbot/EventsListener.java
+++ src/bonusbot/EventsListener.java
@@ -7,6 +7,7 @@ import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.ReadyEvent;
 import sx.blah.discord.handle.impl.events.guild.member.UserJoinEvent;
 import sx.blah.discord.handle.obj.ActivityType;
+import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.StatusType;
 
@@ -39,17 +40,19 @@ class EventsListener {
 	@EventSubscriber
 	public final void onUserJoinedGuild ( final UserJoinEvent event ) {
 		final GuildExtends guildext = GuildExtends.get( event.getGuild() );
-		final Long greetUserChannelID = guildext.getGreetUserChannelID(); 
-		if ( greetUserChannelID != null ) {
+		final IChannel greetUserChannel = guildext.getGreetUserChannel(); 
+		if ( greetUserChannel != null ) {
 			final IGuild guild = event.getGuild();
 			final int amountonserver = guild.getTotalMemberCount();
 			final String suffix = amountonserver == 1 ? "st" : 
 				( amountonserver == 2 ? "nd" :
 				( amountonserver == 3 ? "rd" : "th" ) );
-			Util.sendMessage( guild.getChannelByID( greetUserChannelID ), "Welcome "+event.getUser().mention()
-					+"!\nYou are the "+amountonserver+suffix+" user 🎉"
-					+"\nPlease read 'informations' in 'important' category!" );
-			
+			String welcomemsg = "Welcome "+event.getUser().mention()
+					+"!\nYou are the "+amountonserver+suffix+" user 🎉";
+			IChannel channel = guildext.getInformationsChannel();
+			if ( channel != null )
+				welcomemsg += "\nPlease read " + channel.mention() + " in 'important' category!";
+			Util.sendMessage( greetUserChannel, welcomemsg ); 
 		}
 	}
 }
