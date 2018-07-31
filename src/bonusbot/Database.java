a066b08152e8b010f52d8d1c17334325837eb0b9
emre1702 <emre1702@live.de>
2018-07-31 23:57:24 +0200

    Load MariaDB Driver

--- src/bonusbot/Database.java
+++ src/bonusbot/Database.java
@@ -9,21 +9,25 @@ public class Database {
 	private static String connectionString;
 	
 	public static Connection get() {
-		if (connectionString.equals("-")) {
-			return null;
-		}
 		if (connectionString == null) {
+			try {
+				Class.forName("org.mariadb.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace(Logging.getPrintWrite());
+			}
 			String dbhost = Settings.get("mariaDBHost");
 			int dbport = Settings.get("mariaDBPort");
 			String dbname = Settings.get("mariaDBName");
 			String dbuser = Settings.get("mariaDBUser");
 			String dbpassword = Settings.get("mariaDBPassword");
 			if (dbhost != null && dbuser != null && dbpassword != null && dbname != null && dbport != -1) {
-				connectionString = String.format("jsdbc:mariadb://%s:%d/%s?user=%s&password=%s", dbhost, dbport, dbname, dbuser, dbpassword);
+				connectionString = String.format("jdbc:mariadb://%s:%d/%s?user=%s&password=%s", dbhost, dbport, dbname, dbuser, dbpassword);
 			} else {
 				connectionString = "-";
 				return null;
 			}
+		} else if (connectionString.equals("-")) {
+			return null;
 		}
 		try {
 			Connection conn = DriverManager.getConnection(connectionString);
