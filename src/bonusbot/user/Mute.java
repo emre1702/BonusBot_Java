969f93fedd382e90569121cb44a0400626587d6f
emre1702 <emre1702@live.de>
2018-08-01 18:44:42 +0200

    Add mute role (with DB)

---
+++ src/bonusbot/user/Mute.java
@@ -0,0 +1,25 @@
+package bonusbot.user;
+
+import bonusbot.Database;
+import bonusbot.Settings;
+import bonusbot.guild.GuildExtends;
+import sx.blah.discord.handle.obj.IRole;
+import sx.blah.discord.handle.obj.IUser;
+
+public class Mute {
+	
+	public static void setUserMute(IUser user, GuildExtends guildext, long mutetime, boolean saveindb) {
+		IRole muterole = guildext.getRole(Settings.get("muteRole"));
+		if (muterole != null) {
+			boolean userhasrole = user.hasRole(muterole);
+			if (mutetime != 0 && !userhasrole) {
+				user.addRole(muterole);
+			} else if (userhasrole) {
+				user.removeRole(muterole);
+			}
+		}
+		if (saveindb) {
+			Database.saveUserMute(user, guildext.getGuild(), mutetime);
+		}
+	}
+}
